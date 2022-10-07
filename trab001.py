45024e4bf9cf9f6354838d738e2646ef2f1964c2
KayoBaptista <115320795+KayoBaptista@users.noreply.github.com>
2022-10-07 17:45:24 -0300

    trabalho estacio

--- trab001.py
+++ trab001.py
@@ -1,9 +1,9 @@
-genero = str(input("Insira Homem ou Mulher."))
-altura = float(input('Insira seu altura'))
+sexo = str(input("Insira homem ou mulher."))
+estatura = float(input('Insira seu altura'))
 peso_homem = (altura * 72.7) -58
 peso_mulher = (altura * 62.1)-44.7
 if genero == 'Homem':
     print('Seu peso ideal é:{}'.format(peso_homem))
 else:
     print('Seu peso ideal é:{}'.format(peso_mulher))
-"""Eu apenas atribui valores nesse progama, e utilizei o if or else para a questão do genero(LISTA 1)"""
+"""utilizei if or else e fiz a atribuição de valores(LISTA 1)"""
